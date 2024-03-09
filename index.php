3f8930328856e5891b924e6f62d9f1c8b9fe0348
nico <nicolas.brunelle1@gmail.com>
2024-03-09 21:31:37 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Contient les informations générales sur site (la langue, la fiche de style, les plugins et autres à importer) -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Contenu principal du site -->
+    
+</body>
+</html>
